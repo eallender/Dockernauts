53fbc20a50681ae345b6244a5621b3319f745a1a
Evan Allender <evan.allender@gmail.com>
2025-07-27 19:10:12 -0400

    feat: add a nats client for planets and master station

---
+++ src/utils/nats.py
@@ -0,0 +1,32 @@
+import json
+from nats.aio.client import Client as NATS
+
+class JetStreamClient:
+    def __init__(self, servers="nats://localhost:4222", stream_name="mystream", subject="updates"):
+        self.servers = servers
+        self.stream_name = stream_name
+        self.subject = subject
+        self.nc = NATS()
+        self.js = None
+
+    async def connect(self):
+        await self.nc.connect(servers=[self.servers])
+        self.js = self.nc.jetstream()
+
+    async def publish_json(self, data: dict):
+        message = json.dumps(data)
+        ack = await self.js.publish(self.subject, message.encode())
+        print(f"Published to {self.subject}, seq={ack.seq}")
+
+    async def subscribe_json(self, callback):
+        async def message_handler(msg):
+            try:
+                decoded = json.loads(msg.data.decode())
+                await callback(decoded)
+            except json.JSONDecodeError:
+                print(f"Invalid JSON: {msg.data}")
+
+        await self.js.subscribe(self.subject, cb=message_handler, durable="durable")
+
+    async def close(self):
+        await self.nc.close()
